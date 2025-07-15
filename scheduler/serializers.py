62859c084afc03d856bc81fafdc1450866d8b4bf
reutDayan1 <reut19991@gmail.com>
2025-07-15 11:11:04 +0300

    create model for USER and LESSONS

--- scheduler/serializers.py
+++ scheduler/serializers.py
@@ -1,6 +1,30 @@
 from rest_framework import serializers
 from .models import Lesson
 
+from .models import User
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password', 'role']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data.get('email', ''),
+            password=validated_data['password'],
+            role=validated_data['role'],
+            age=validated_data['age'],
+            subject=validated_data['subject'],
+            points=validated_data['role'],
+            phone=validated_data['phone']
+        )
+        return user
+    
+
+# Serializer that converts Lesson model instances to JSON and vice versa,
+# enabling creation, retrieval, and update of lessons through the API
 class LessonSerializer(serializers.ModelSerializer):
     class Meta:
         model = Lesson
--- scheduler/urls.py
+++ scheduler/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import LessonViewSet
+from .views import LessonViewSet ,UserRegistrationView
 
 # Create a router and register our viewset with it
 router = DefaultRouter()
@@ -8,5 +8,6 @@ router.register(r'lessons', LessonViewSet)
 
 # Include the router's URLs in this app's URLs
 urlpatterns = [
+     path('api/register/', UserRegistrationView.as_view(), name='user-register'),
     path('', include(router.urls)),
 ]
