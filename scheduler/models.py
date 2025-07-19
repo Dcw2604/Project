a8f96f251cd83566aba86ff8a9b9e915bd0a05a2
reutDayan1 <reut19991@gmail.com>
2025-07-19 09:39:48 +0300

    new push of chat integration

--- scheduler/models.py
+++ scheduler/models.py
@@ -33,3 +33,18 @@ class Lesson(models.Model):
         )
     status = models.CharField(max_length=10, choices=status_CHOICES, default='pending')
 
+#שמירה השיחות עם הצאט
+class ChatInteraction(models.Model):
+    student = models.ForeignKey(User, on_delete=models.CASCADE, limit_choices_to={'role':'student'})
+    question = models.TextField()
+    answer = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+  # ניתוח עתידי
+    topic = models.CharField(max_length=100, blank=True, null=True)  # למשל: "אלגברה", "גיאומטריה"
+    difficulty_estimate = models.CharField(max_length=50, blank=True, null=True)  # למשל: "קל", "בינוני", "קשה"
+    notes = models.TextField(blank=True, null=True)  # למשל תובנות מהצ'אט
+
+    #הצגת המשתמש והשעה שבה הוא שלח הודעה
+    def __str__(self):
+        return f"{self.student.username} - {self.created_at.strftime('%Y-%m-%d %H:%M:%S')}"
